aefaf646217c7d9f4b82849d4b42099ac08dcf59
Minsu <minsu.kim08@gmail.com>
2020-05-20 14:02:13 +0900

    1150 done!

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,8 +1,12 @@
 package com.leetcode;
 
+import com.leetcode.problem1150.Solution1150;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Solution1150 solution1150 = new Solution1150();
+        int result = solution1150.numRollsToTarget(2,6,7);
+        System.out.println(result);
     }
 }
