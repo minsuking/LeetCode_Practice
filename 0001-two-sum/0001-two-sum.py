97981d7b1802dd8e15f22193f2d58e34bf1418c4
Minsu Kim <60283244+minsuking@users.noreply.github.com>
2024-01-31 09:48:03 +0900

    Time: 60 ms (73.69%), Space: 17.7 MB (73.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         maps_ = dict()
-        for i, v in enumerate(nums):
-            if target-v in maps_:
-                return [maps_[target-v],i]
+        for i, n in enumerate(nums):
+            if target-n in maps_:
+                return [maps_[target-n], i]
             else:
-                maps_[v] = i
+                maps_[n] = i
                 
                 
             
