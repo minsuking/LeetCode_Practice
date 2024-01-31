f09676d7f98cd15633f96753aed2e701b889450f
Minsu Kim <60283244+minsuking@users.noreply.github.com>
2024-01-31 09:47:33 +0900

    Time: 64 ms (61.04%), Space: 17.9 MB (58.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,11 @@
-from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map_ = defaultdict(int)
-        for i, n in enumerate(nums):
-            if n in map_ and n * 2 == target:
-                return [map_[n], i]
-            map_[n] = i
-
-        
-        for i, n in enumerate(nums):
-            if target - n in map_ and map_[target - n] != i:
-                return [map_[target - n], i]
+        maps_ = dict()
+        for i, v in enumerate(nums):
+            if target-v in maps_:
+                return [maps_[target-v],i]
+            else:
+                maps_[v] = i
                 
                 
             
