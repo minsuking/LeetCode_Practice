563c3434257025099006ea2f2e475c3fc6244fc9
Minsu Kim <60283244+minsuking@users.noreply.github.com>
2024-02-06 13:07:08 +0900

    Time: 67 ms (72.31%), Space: 17 MB (70.16%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -0,0 +1,23 @@
+class Codec:
+    def __init__(self):
+        self.key = chr(257) + "271" + chr(257)
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        res = ""
+        for s in strs:
+            res += s + self.key
+        return res
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        res = s.split(self.key)
+        res.pop()
+        return res
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
