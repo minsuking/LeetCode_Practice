3e0ac932c343b77bae7401f91eb8ac3e52591ecd
Minsu Kim <60283244+minsuking@users.noreply.github.com>
2024-02-08 13:23:51 +0900

    Time: 5734 ms (5.61%), Space: 31.9 MB (69.36%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -3,13 +3,13 @@ class Solution:
         if len(nums) < 2:
             return len(nums)
         
-        nums = set(nums)
-        ans = 0
+        check = set(nums)
+        res = 0
         for n in nums:
-            if n-1 not in nums:
+            if n-1 not in check:
                 cnt = 1
-                while n+1 in nums:
+                while n+1 in check:
                     cnt += 1
                     n += 1
-                ans = max(ans,cnt)
-        return ans
+                res = max(res,cnt)
+        return res
